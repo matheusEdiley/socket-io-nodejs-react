b55796d05b5188a80f5ba1fc787583e9ae0f1c2f
Matheus <matheus@ubuntu.ubuntu-domain>
2018-07-22 16:35:46 -0300

    update

---
+++ client/src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client'
+import {
+  Button, Panel, Form, FormControl, FormGroup,
+  Well, Col, ControlLabel, Glyphicon
+} from 'react-bootstrap';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "http://localhost:5000",
+      message: '',
+      mgsReceived: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // sending sockets
+  send = () => {
+    console.log(this.state.message);
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('msg', this.state.message)
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state);
+  }
+
+  render() {
+
+    // testing for socket connections
+    const socket = socketIOClient(this.state.endpoint);
+
+    socket.on('msg', (msg) => {
+      this.state.mgsReceived = msg;
+      this.setState({});
+    })
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Socket.IO</h1>
+        </header>
+
+        <Panel className="App-form">
+          <h3> <Glyphicon glyph="transfer" /> </h3>
+          <Form id="genericForm" horizontal>
+
+            <div>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Message
+                </Col>
+                <Col sm={10}>
+                  <FormControl name="message" onChange={this.handleChange}
+                    placeholder="Enter a message" />
+                </Col>
+              </FormGroup>
+              <Button className="App-button" bsStyle="primary" onClick={() => this.send()}> Send <Glyphicon glyph="send" />
+              </Button>
+            </div>
+          </Form>
+        </Panel>
+        {this.state.mgsReceived.length > 0 ?
+          <Panel className="App-form">
+            <h3> <Glyphicon glyph="comment" /> </h3>
+            {<Well bsSize="large" >{this.state.mgsReceived}</Well>}
+          </Panel>: ""
+        }
+
+      </div>
+    )
+  }
+}
+export default App;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+// our localhost port
+const port = 5000;
+
+const app = express();
+
+// our server instance
+const server = http.createServer(app);
+
+// This creates our socket using the instance of the server
+const io = socketIO(server);
+
+// This is what the socket.io syntax is like, we will work this later
+io.on('connection', socket => {
+  console.log(`New client connected id: ${socket.id}`);
+  
+  // just like on the client side, we have a socket.on method that takes a callback function
+  socket.on('msg', (msg) => {
+   
+    console.log('Message Changed to: ', msg);
+    io.sockets.emit('msg', msg);
+  })
+  
+  // disconnect is fired when a client leaves the server
+  socket.on('disconnect', () => {
+    console.log(`user disconnected id: ${socket.id}`);
+  })
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
